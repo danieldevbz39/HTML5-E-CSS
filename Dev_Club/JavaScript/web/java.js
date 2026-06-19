9e2f8ae0a1eb1e4a6875cfc3d396fc9cf61affd2
danieldevbz39 <adsdaniel85@gmail.com>
2026-06-19 18:38:24 -0300

    funções e eventos

--- Dev_Club/JavaScript/web/java.js
+++ Dev_Club/JavaScript/web/java.js
@@ -8,12 +8,65 @@
     querySelectorAll -> Trás todos os elementos que encontrar
 */
 
-const input = document.getElementById("main-input")
-const elements = document.getElementsByClassName("paragraph-js")
-const element = document.getElementsByTagName("h1")
-const elementn = document.getElementsByName("nome-completo")
+// const input = document.getElementById("main-input")
+// const elements = document.getElementsByClassName("paragraph-js")
+// const element = document.getElementsByTagName("h1")
+// const elementn = document.getElementsByName("nome-completo")
 
-const elementsp = document.querySelector(".paragraph-js")
-const elementtodos = document.querySelectorAll("pargraph-js")
+//-----------------------------------_-_--------------------------
 
-console.log(input, elements,element,elementn,elementsp)
+// const elementsp = document.querySelector(".paragraph-js")
+// const elementtodos = document.querySelectorAll("pargraph-js")
+
+// const element = document.querySelector("#main-input") // criando um elemento
+
+// element.value = "Digite aqui!" //adicinando valor no campo
+// console.log(element.placeholder) //exibindo no console
+
+//-------------------------------_-_--------------------------------
+
+// Alterando e acessndo textos
+
+//textContent -> Pega todo conteúdo
+//innerText -> Pega apenas o texto
+//innerHTML -> Permite adicionar HTML e texto
+
+// const element = document.querySelector(".paragraph-js")
+
+
+// // element.innerText = "Novo texto"  // alterando texto
+// console.log(element.textContent) // Só html
+// console.log(element.innerText) // Leva em conta o css
+// console.log(element.innweHTML) // Trás tudo
+
+//-------------------------------_-_--------------------------------
+
+// const button = document.querySelector(".main-button")
+
+// button.style.color = "white" //trocando cor
+// button.style.backgroundColor = "red" //alterrando cor do fundo botão
+
+
+//-------------------------------_-_--------------------------------
+
+// EVENTOS - Funções
+
+// const input = document.querySelector("#main-input")
+
+// function cliqueiNoBotao(){
+//     alert("Botão clicado com sucesso!")
+// }
+
+// function digiteiNoInput(){
+//     console.log(input.value)
+// }
+
+const input = document.querySelector("#main-input")
+
+function cliqueiNoBotao(){
+    console.log(input.value)
+}
+
+function digiteiNoInput(){
+    console.log("Digitei no input")
+}
