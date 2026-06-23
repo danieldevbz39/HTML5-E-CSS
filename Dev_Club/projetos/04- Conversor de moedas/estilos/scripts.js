47b863d55df01e6ea8c06883a7cbd25adb0855f9
danieldevbz39 <adsdaniel85@gmail.com>
2026-06-23 18:29:26 -0300

    adicionando conversão

--- Dev_Club/projetos/04- Conversor de moedas/estilos/scripts.js
+++ Dev_Club/projetos/04- Conversor de moedas/estilos/scripts.js
@@ -2,10 +2,24 @@ const convertButton = document.querySelector(".convert-button")
 
 function convertValues(){
     const inputCurrencyValue = document.querySelector(".input-currency").value
-    
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")  // Valor em Real
+    const currencyValueConverted = document.querySelector(".currency-value")  // Valor convertido
+
+
     const dolarToday = 5.2
 
     const convertedValue = inputCurrencyValue / dolarToday
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("es-US", {
+        style: "currency",
+        currency: "USD"
+    }).format(convertedValue)
+
 
     console.log(convertedValue)
 }
