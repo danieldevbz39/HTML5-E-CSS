b31c6de3da652dbddbd9762b3f24884190e861f6
danieldevbz39 <adsdaniel85@gmail.com>
2026-06-23 18:57:59 -0300

    adicionando euro

--- Dev_Club/projetos/04- Conversor de moedas/estilos/scripts.js
+++ Dev_Club/projetos/04- Conversor de moedas/estilos/scripts.js
@@ -1,28 +1,41 @@
-const convertButton = document.querySelector(".convert-button")
+const convertButton = document.querySelector(".convert-button");
+const currencySelect = document.querySelector(".currency-select");
 
-function convertValues(){
-    const inputCurrencyValue = document.querySelector(".input-currency").value
-    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")  // Valor em Real
-    const currencyValueConverted = document.querySelector(".currency-value")  // Valor convertido
+function convertValues() {
+  const inputCurrencyValue = document.querySelector(".input-currency").value;
+  const currencyValueToConvert = document.querySelector(
+    ".currency-value-to-convert",
+  ); // Valor em Real
+  const currencyValueConverted = document.querySelector(".currency-value"); // Valor convertido
 
+  console.log(currencySelect.value);
+  const dolarToday = 5.2;
+  const euroToday = 6.2;
 
-    const dolarToday = 5.2
+  const convertedValue = inputCurrencyValue / dolarToday;
 
-    const convertedValue = inputCurrencyValue / dolarToday
-
-    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
-        style: "currency",
-        currency: "BRL"
-    }).format(inputCurrencyValue)
-
+  if (currencySelect.value == "dolar") {
+    // Se o select estiver selecionado o valor de dolar, entre aqui
     currencyValueConverted.innerHTML = new Intl.NumberFormat("es-US", {
-        style: "currency",
-        currency: "USD"
-    }).format(convertedValue)
+      style: "currency",
+      currency: "USD"
+    }).format(convertedValue);
+  }
 
+  if (currencySelect.value == "euro") {
+    // Se o select estiver selecionado o valor de dolar, entre aqui
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "EUR"
+    }).format(inputCurrencyValue/euroToday)
+  }
 
-    console.log(convertedValue)
+  currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(inputCurrencyValue);
+
+  console.log(convertedValue);
 }
 
-
-convertButton.addEventListener("click", convertValues)
+convertButton.addEventListener("click", convertValues);
