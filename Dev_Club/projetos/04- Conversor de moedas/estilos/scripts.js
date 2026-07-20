1eeb5d2e73a7a63fefbba0ad16e146488c4a9005
danieldevbz39 <adsdaniel85@gmail.com>
2026-07-20 15:03:12 -0300

    conversor de moedas

--- Dev_Club/projetos/04- Conversor de moedas/estilos/scripts.js
+++ Dev_Club/projetos/04- Conversor de moedas/estilos/scripts.js
@@ -27,14 +27,12 @@
 
 // //----------------------- Convertendo valores dos campos  --------------------
 
-
 // const convertButton = document.querySelector(".convert-button");
 
 // function convertValues() {// criando função
 //   const inputCurrencyValue = document.querySelector(".input-currency").value
 //   const currencyValueToConvert = document.querySelector(".currency-value-to-convert") //valor em real
 //   const currencyValueConverted = document.querySelector(".currency-value") //outras moedas
-
 
 //   const dolarToday = 5.2
 
@@ -46,62 +44,89 @@
 
 // convertButton.addEventListener("click", convertValues) //convertendo click e valor
 
-
 //-------------------------------------- Formatando valor convertido ---------------------------------------------------
-
 
 const convertButton = document.querySelector(".convert-button");
 const currencySelect = document.querySelector(".currency-select");
 
-function convertValues() {// criando função
-  const inputCurrencyValue = document.querySelector(".input-currency").value
-  const currencyValueToConvert = document.querySelector(".currency-value-to-convert") //valor em real
-  const currencyValueConverted = document.querySelector(".currency-value") //outras moedas
+function convertValues() {
+  // criando função
+  const inputCurrencyValue = document.querySelector(".input-currency").value;
+  const currencyValueToConvert = document.querySelector(
+    ".currency-value-to-convert",
+  ); //valor em real
+  const currencyValueConverted = document.querySelector(".currency-value"); //outras moedas
 
-  const dolarToday = 5.2
-  const euroToday = 5.87
-  const libraToday = 6.87
-  const btcToday = 329816.83
+  const dolarToday = 5.2;
+  const euroToday = 5.87;
+  const libraToday = 6.87;
+  const btcToday = 329816.83;
 
   if (currencySelect.value == "dolar") {
     currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
       style: "currency",
-      currency: "USD"
-    }).format(inputCurrencyValue / dolarToday)  //convertendo em OUTRA MOEDA
+      currency: "USD",
+    }).format(inputCurrencyValue / dolarToday); //convertendo em OUTRA MOEDA
   }
 
   if (currencySelect.value == "euro") {
     currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
       style: "currency",
-      currency: "EUR"
-    }).format(inputCurrencyValue / euroToday)  //convertendo em OUTRA MOEDA
+      currency: "EUR",
+    }).format(inputCurrencyValue / euroToday); //convertendo em OUTRA MOEDA
   }
 
   if (currencySelect.value == "libra") {
     currencyValueConverted.innerHTML = new Intl.NumberFormat("uk-UK", {
       style: "currency",
-      currency: "GBP"
-    }).format(inputCurrencyValue / libraToday)
+      currency: "GBP",
+    }).format(inputCurrencyValue / libraToday);
   }
 
   if (currencySelect.value == "btc") {
     currencyValueConverted.innerHTML = new Intl.NumberFormat("pp-PP", {
       style: "currency",
-      currency: "BTC"
-    }).format(inputCurrencyValue / btcToday)
+      currency: "BTC",
+    }).format(inputCurrencyValue / btcToday);
   }
 
   currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
     style: "currency",
-    currency: "BRL"
-  }).format(inputCurrencyValue) //pegando valor convertido
+    currency: "BRL",
+  }).format(inputCurrencyValue); //pegando valor convertido
 
   currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
     style: "currency",
-    currency: "USD"
-  }).format(convertedValue)  //convertendo em OUTRA MOEDA
+    currency: "USD",
+  }).format(convertedValue); //convertendo em OUTRA MOEDA
 }
 
-convertButton.addEventListener("click", convertValues) //convertendo click e valor
+function changeCurrency() {
+  const currencyName = document.getElementById("currency-name");
+  const currencyImg = document.querySelector(".currency-img");
 
+  if (currencySelect.value == "dolar") {
+    currencyName.innerHTML = "Dólar americano";
+    currencyImg.src = "./assets/dolar.png";
+  }
 
+  if (currencySelect.value == "euro") {
+    currencyName.innerHTML = "Euro";
+    currencyImg.src = "./assets/euro.png";
+  }
+
+  if (currencySelect.value == "libra") {
+    currencyName.innerHTML = "Libra";
+    currencyImg.src = "./assets/libra.png";
+  }
+
+  if (currencySelect.value == "btc") {
+    currencyName.innerHTML = "Bitcoin";
+    currencyImg.src = "./assets/btc.png";
+  }
+  convertValues ()
+}
+
+currencySelect.addEventListener("change", changeCurrency);
+
+convertButton.addEventListener("click", convertValues); //convertendo click e valor
