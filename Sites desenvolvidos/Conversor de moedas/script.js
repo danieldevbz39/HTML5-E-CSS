e84bf5a56f6866c1786761bc8952e858bb39feae
danieldevbz39 <adsdaniel85@gmail.com>
2026-07-22 19:22:31 -0300

    conversor de moedas 2.0

--- Sites desenvolvidos/Conversor de moedas/script.js
+++ Sites desenvolvidos/Conversor de moedas/script.js
@@ -1,1 +1,13 @@
-const amountInput = document.getElementById('amount')
+const amountInput = document.querySelector('#amount') //Values
+const fromSelect = document.querySelector('fromCurrency')//De...
+const toSelect = document.querySelector('toCurrency')//Para...
+const convertBtn = document.querySelector('#convertBtn') //Botão
+const resultText = document.querySelector('result')//Resutado 
+
+    function convertValues (){
+        const amountInput = document.querySelector('#amount').value
+        console.log(amountInput)
+    }
+
+
+convertBtn.addEventListener('click', convertValues)
